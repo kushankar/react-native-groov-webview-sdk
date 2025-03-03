8c21f6e9464f6937cc35e43fd5741eb685128155
kushankar <91423364+kushankar@users.noreply.github.com>
2025-03-03 19:13:18 +0530

    New URI2 added

--- config.js
+++ config.js
@@ -1,2 +1,3 @@
 export const GROOV_URI = 'https://app.wearegroov.io/api/v1/emca/merchant-identity';
 export const GROOV_AUTH_KEY = 'x-api-key';
+export const GROOV_URI2 = 'https://app.wearegroov.io/api/v1/emca/generate-merchant-token';
